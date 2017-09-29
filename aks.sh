59b97266397785f5483b1a359ae87cc17990fa1d
Daniel Falkner <dafalkne@microsoft.com>
2017-09-29 15:40:54 +0200

    AKS + Special Version of azure-cli docker image

---
+++ aks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. ./util.sh
+
+run "clear"
+
+desc "Create an AKS (Azure Kubernetes Service)"
+run "az group create --name AKS --location ukwest"
+run "az aks create --name k8sukwest --resource-group AKS --location ukwest"
+
+desc "Getting credentials for the k8s cluster"
+run "az aks get-credentials --name k8sukwest --resource-group AKS"
+run "kubectl config set-cluster k8sukwest --insecure-skip-tls-verify=true"
+
+run "kubectl run architects-forum --image=dfalkner/architectsforum"
+
+run "az group delete --name AKS"
---
+++ aks/bootstrap.sh
@@ -0,0 +1,11 @@
+cd /tmp
+wget http://www.ivarch.com/programs/sources/pv-1.6.6.tar.gz
+tar xfvz pv-1.6.6.tar.gz
+cd pv-1.6.6
+sh ./configure
+make
+make install
+apk update
+apk add ncurses util-linux git
+cd /root
+git clone https://github.com/derdanu/architects-forum.git

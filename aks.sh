c1f20025fa02c99a591450a639208de2cc995882
Daniel Falkner <dafalkne@microsoft.com>
2017-09-29 16:53:10 +0200

    k8s sample deployment + service

--- aks.sh
+++ aks.sh
@@ -11,6 +11,8 @@ desc "Getting credentials for the k8s cluster"
 run "az aks get-credentials --name k8sukwest --resource-group AKS"
 run "kubectl config set-cluster k8sukwest --insecure-skip-tls-verify=true"
 
-run "kubectl run architects-forum --image=dfalkner/architectsforum"
+desc "Deploying stuff on the cluster"
+run "cat aks/deployment.yaml"
+run "kubectl create -f aks/deployment.yaml"
 
 run "az group delete --name AKS"

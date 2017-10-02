a67995fcf4d3f5935d48af174cee42724f54f298
Daniel Falkner <dafalkne@microsoft.com>
2017-10-02 10:49:40 +0200

    more demo

--- aks/demo.sh
+++ aks/demo.sh
@@ -10,5 +10,37 @@ run "kubectl config set-cluster k8sukwest --insecure-skip-tls-verify=true"
 desc "Deploying stuff on the cluster"
 run "cat deployment.yaml"
 run "kubectl create -f deployment.yaml"
+run "kubectl get svc"
+
+ip=$(kubectl get svc architects-forum  -o json | jq .status.loadBalancer.ingress[0].ip)
+while [[ "${ip}" == "null" ]]; do
+  ip=$(kubectl get svc architects-forum  -o json | jq .status.loadBalancer.ingress[0].ip)
+  echo "waiting for ip..."
+  sleep 1
+done
+
+temp="${ip%\"}"
+temp="${temp#\"}"
+
+ip=${temp}
+
+while ! curl -s --connect-timeout 1 ${ip} > /dev/null; do
+  echo "waiting for container..."
+  sleep 1
+done
+
+desc "Server is running at http://${ip}"
+
+run "curl ${ip}"
+run "kubectl get pods -o wide"
+
+pod=$(kubectl get pods -o json | jq .items[0].metadata.name)
+
+run "kubectl delete pod ${pod}"
+run "kubectl get pods -o wide"
+
+run "kubectl scale deployment architects-forum-deployment --replicas=10"
+run "kubectl get pods -o wide"
+run "kubectl get pods -o wide"
 
 run "az group delete --name AKS --no-wait"

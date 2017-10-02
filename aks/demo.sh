176980b87ca182e92e8b2424803b805b91fb3019
Daniel Falkner <dafalkne@microsoft.com>
2017-10-02 10:36:24 +0200

    dir fix

--- aks/demo.sh
+++ aks/demo.sh
@@ -9,6 +9,6 @@ run "kubectl config set-cluster k8sukwest --insecure-skip-tls-verify=true"
 
 desc "Deploying stuff on the cluster"
 run "cat deployment.yaml"
-run "kubectl create -f aks/deployment.yaml"
+run "kubectl create -f deployment.yaml"
 
 run "az group delete --name AKS --no-wait"

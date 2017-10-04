0ab42d80043ce9831e97ae417643b58f06856e86
Daniel Falkner <dafalkne@microsoft.com>
2017-10-04 10:01:45 +0200

    ACR demo

---
+++ acr/demo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+. ../util.sh
+
+run "clear"
+cd ../www
+desc "Login to Azure Container Registry"
+#run "docker login architectsforum.azurecr.io"
+desc "Build and push the image"
+run "docker build . -t architectsforum.azurecr.io/architects-forum"
+run "docker push architectsforum.azurecr.io/architects-forum"
+
+desc "Modify the image and publish it again"
+run "sed -i 's/Munich/New York/' index.php"
+run "docker build . -t architectsforum.azurecr.io/architects-forum"
+run "docker push architectsforum.azurecr.io/architects-forum"
+
+git checkout index.php
+
